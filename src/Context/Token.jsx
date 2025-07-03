197332a6ac8f15820164bb86fc1fc3e186d04ffe
Ahmed_AlSliny <www.ahmedshehata604@gmail.com>
2025-07-03 20:45:43 +0300

    Update Token.jsx

--- src/Context/Token.jsx
+++ src/Context/Token.jsx
@@ -1,13 +1,13 @@
-import React, { createContext, useState } from 'react'; // تم استيراد createContext  
+import React, { createContext, useState } from 'react'; 
 
-// إنشاء السياق (Context) بشكل صحيح  
-export const TokenContext = createContext(null); // تم تصحيح الاسم وتم استخدام createContext  
+
+export const TokenContext = createContext(null);
 
 export default function TokenContextProvider(props) { 
 
     const [token, setToken] = useState(null);  
 
-    // توفير القيمة (value) التي تحتوي على token و setToken  
+    
     return <TokenContext.Provider value={{token , setToken }}>
          {props.children}
          </TokenContext.Provider> 
